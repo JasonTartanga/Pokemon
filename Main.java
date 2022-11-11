caf14ea9e6edc7694f1d0f28c49f8215180c4349
Jason <113925541+JasonTartanga@users.noreply.github.com>
2022-11-11 14:02:50 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,20 +6,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		int opc;
-		do {
-			String[] movimientosCharizard = { "Lanzallamas", "Ataque Ala", "Respiro", "Fuego Fatuo" };
-			String[] pokemons = { "Charizard", "Greninja", "Decidueye" };
-			String[] opciones = { "Cambiar Pokemon", "Atacar" };
+		int sacar = 0;
 
-			opc = JOptionPane.showOptionDialog(null, "¿Que quieres hacer?", "nombrePokemon", 0, 3, null, opciones,
-					0);
+		Movimientos movimientos = new Movimientos();
+		Pokemons pokemon = new Pokemons();
+
+		do {
+
+			opc = JOptionPane.showOptionDialog(null, "¿Que quieres hacer?", pokemon.pokemons[sacar], 0, 3, null,
+					pokemon.opciones, 0);
 
 			switch (opc) {
 			case 0:
-				cambiarPokemon(movimientosCharizard, pokemons, opciones);
+				sacar = cambiarPokemon(sacar, pokemon.pokemons, pokemon.opciones);
 				break;
 			case 1:
-				atacar(movimientosCharizard, pokemons, opciones);
+				atacar(sacar, pokemon.pokemons, pokemon.opciones, movimientos.charizard, movimientos.greninja,
+						movimientos.decidueye);
 				break;
 			case -1:
 				JOptionPane.showMessageDialog(null, "Eres un pussy");
@@ -30,50 +33,96 @@ public class Main {
 		} while (opc != -1);
 	}
 
-	private static void atacar(String[] movimientosCharizard, String[] pokemons, String[] opciones) {
-		int opc2;
-		opc2 = JOptionPane.showOptionDialog(null, "", "¿A que pokemon quieres cambiar?", 0, -1, null,
-				movimientosCharizard, 0);
+	private static void atacar(int sacar, String[] pokemons, String[] opciones, String[] charizard, String[] greninja,
+			String[] decidueye) {
 
-		switch (opc2) {
-		case 0:
-			JOptionPane.showMessageDialog(null, "Has usado ataque1");
-			return;
-		case 1:
-			JOptionPane.showMessageDialog(null, "Has usado ataque2");
-			return;
-		case 2:
-			JOptionPane.showMessageDialog(null, "Has usado ataque3");
-			return;
-		case 3:
-			JOptionPane.showMessageDialog(null, "Has usado ataque4");
-			return;
-		case -1:
-		default:
-			return;
+		int opc2;
+
+	
+		if (sacar == 0) {
+			opc2 = JOptionPane.showOptionDialog(null, "", "¿Que movimiento quieres usar?", 0, -1, null, charizard, 0);
+			switch (opc2) {
+			case 0:
+				JOptionPane.showMessageDialog(null, "Has usado " + charizard[opc2]);
+				return;
+			case 1:
+				JOptionPane.showMessageDialog(null, "Has usado" + charizard[opc2]);
+				return;
+			case 2:
+				JOptionPane.showMessageDialog(null, "Has usado" + charizard[opc2]);
+				return;
+			case 3:
+				JOptionPane.showMessageDialog(null, "Has usado " + charizard[opc2]);
+				return;
+			case -1:
+			default:
+				return;
+			}
+		} else if (sacar == 1) {
+			opc2 = JOptionPane.showOptionDialog(null, "", "¿Que movimiento quieres usar?", 0, -1, null, greninja, 0);
+			switch (opc2) {
+			case 0:
+				JOptionPane.showMessageDialog(null, "Has usado " + greninja[opc2]);
+				return;
+			case 1:
+				JOptionPane.showMessageDialog(null, "Has usado" + greninja[opc2]);
+				return;
+			case 2:
+				JOptionPane.showMessageDialog(null, "Has usado" + greninja[opc2]);
+				return;
+			case 3:
+				JOptionPane.showMessageDialog(null, "Has usado " + greninja[opc2]);
+				return;
+			case -1:
+			default:
+				return;
+			}
+		} else {
+			opc2 = JOptionPane.showOptionDialog(null, "", "¿Que movimiento quieres usar?", 0, -1, null, decidueye, 0);
+			switch (opc2) {
+			case 0:
+				JOptionPane.showMessageDialog(null, "Has usado " + decidueye[opc2]);
+				return;
+			case 1:
+				JOptionPane.showMessageDialog(null, "Has usado" + decidueye[opc2]);
+				return;
+			case 2:
+				JOptionPane.showMessageDialog(null, "Has usado" + decidueye[opc2]);
+				return;
+			case 3:
+				JOptionPane.showMessageDialog(null, "Has usado " + decidueye[opc2]);
+				return;
+			case -1:
+			default:
+				return;
+			}
 		}
+
 	}
 
-	private static void cambiarPokemon(String[] movimientosCharizard, String[] pokemons, String[] opciones) {
+	private static int cambiarPokemon(int sacar, String[] pokemons, String[] opciones) {
 		int opc1;
 		opc1 = JOptionPane.showOptionDialog(null,
-				"Pokemon1: nombre\nPS1: nombre\nPokemon2: nombre\nPS2: nombre\nPokemon3: nombre\nPS3: nombre",
+				pokemons[0] + "\nPS: \n" + pokemons[1] + "\nPS: \n" + pokemons[2] + "\nPS: \n",
 				"¿A que pokemon quieres cambiar?", 0, 3, null, pokemons, 0);
 
 		switch (opc1) {
 		case 0:
-			JOptionPane.showMessageDialog(null, "Has sacado a Pokemon1");
-			return;
+			JOptionPane.showMessageDialog(null, "Has sacado a Charizard");
+			sacar = 0;
+			break;
 		case 1:
-			JOptionPane.showMessageDialog(null, "Has sacado a Pokemon2");
-			return;
+			JOptionPane.showMessageDialog(null, "Has sacado a Greninja");
+			sacar = 1;
+			break;
 		case 2:
-			JOptionPane.showMessageDialog(null, "Has sacado a Pokemon3");
-			return;
+			JOptionPane.showMessageDialog(null, "Has sacado a Decidueye");
+			sacar = 2;
+			break;
 		case -1:
 		default:
-			return;
-
+			break;
 		}
+		return sacar;
 	}
 }
